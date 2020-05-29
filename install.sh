b161de2cc1aee27b7b1c84d62ddada0204e09111
sabregreen <tharpj@gmail.com>
2020-05-29 17:44:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,14 +19,14 @@ brew uninstall --ignore-dependencies usbmuxd
 # Install latest usbmux daemon
 brew install --HEAD usbmuxd
 
-# Recreate usbmux daemon link
-brew unlink usbmuxd && brew link usbmuxd
+# Unlink libplist
+brew unlink libplist
+
+# Install latest libplist
+brew install --HEAD libplist
 
 # Install latest libimobiledevice
 brew install --HEAD libimobiledevice
-
-# Recreate ilibmobiledevice link
-brew unlink libimobiledevice && brew link libimobiledevice
 
 # Install latest idevice installer
 brew install --HEAD ideviceinstaller
